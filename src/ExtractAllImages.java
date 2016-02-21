3ee1d268a0a51900a0c248e2c94c7d6906b7ec0d
iAmAn00b <Parthe1989@einrot.com>
2016-02-21 12:19:46 +0100

    widkfasdklfjañsdd ! !"·$

---
+++ src/ExtractAllImages.java
@@ -0,0 +1,74 @@
+import javax.imageio.ImageIO;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ExtractAllImages {
+    public ExtractAllImages () {
+    }
+
+    public void execute() {
+        ExtractAllImages.run();
+    }
+
+
+        public static void run() {
+
+            String webUrl = "http://www.hdwallpapers.in/";
+            URL url = new URL(webUrl);
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            HTMLEditorKit htmlKit = new HTMLEditorKit();
+            HTMLDocument htmlDoc = (HTMLDocument) htmlKit.createDefaultDocument();
+            HTMLEditorKit.Parser parser = new ParserDelegator();
+            HTMLEditorKit.ParserCallback callback = htmlDoc.getReader(0);
+            parser.parse(br, callback, true);
+
+            for (HTMLDocument.Iterator iterator = htmlDoc.getIterator(HTML.Tag.IMG); iterator.isValid(); iterator.next()) {
+                AttributeSet attributes = iterator.getAttributes();
+                String imgSrc = (String) attributes.getAttribute(HTML.Attribute.SRC);
+
+                if (imgSrc != null && (imgSrc.endsWith(".jpg") || (imgSrc.endsWith(".png")) || (imgSrc.endsWith(".jpeg")) || (imgSrc.endsWith(".bmp")) || (imgSrc.endsWith(".ico")))) {
+                    try {
+                        downloadImage(webUrl, imgSrc);
+                    } catch (IOException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            }
+        }
+        private static void downloadImage(String url, String imgSrc) throws IOException {
+            BufferedImage image = null;
+            try {
+                if (!(imgSrc.startsWith("http"))) {
+                    url = url + imgSrc;
+                } else {
+                    url = imgSrc;
+                }
+                imgSrc = imgSrc.substring(imgSrc.lastIndexOf("/") + 1);
+                String imageFormat = null;
+                imageFormat = imgSrc.substring(imgSrc.lastIndexOf(".") + 1);
+                String imgPath = null;
+                imgPath = "C:/Users/Machine2/Desktop/CTE/Java-WebsiteRead/" + imgSrc + "";
+                URL imageUrl = new URL(url);
+                image = ImageIO.read(imageUrl);
+                if (image != null) {
+                    File file = new File(imgPath);
+                    ImageIO.write(image, imageFormat, file);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+}
